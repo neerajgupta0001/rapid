5666fc3f9b84ff60227a9c8a488ed06ea26fe26e
Parveen Bhadoo <35890959+ParveenBhadooOfficial@users.noreply.github.com>
2018-02-11 20:06:13 +0530

    Update header.php

--- templates/plugmod/header.php
+++ templates/plugmod/header.php
@@ -30,7 +30,7 @@ if ($GLOBALS['options']['flist_sort']) { echo '<script type="text/javascript" sr
 </head>
 
 <body>
-<center><img src="//cdn.bhadoo.net/bhadoo-net-logo-transparent.png" alt="RapidLeech PlugMod" border="0" /></center>
+<center><a href="/"><img border="0" alt="Bhadoo" src="//cdn.bhadoo.net/bhadoo-net-logo-transparent.png" width="193" height="35"></a></center>
 <center><script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <!-- 728x90 -->
 <ins class="adsbygoogle"
